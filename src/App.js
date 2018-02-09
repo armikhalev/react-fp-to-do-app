f4f207b740844b0028399174cc14a389e861bba1
armikhalev <armikhalev@gmail.com>
2018-02-08 23:08:17 -0500

    Refact: let input be in App.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,67 @@
 import React, { Component } from 'react';
 import './App.css';
+import { createItem, itemComp, addItem } from './src/item';
 import { ListItems } from './src/list-items';
-import { InputForm } from './src/input';
 
-// TEMP: should be replaced by real data
-const ITEMS = [ 1, 2, 3 ]
-	.map(num => (
-		{
-			name: 'item' + num,
-			checked: false
-		})
-	)
-;
+// // TEMP: should be replaced by real data
+// const ITEMS = [ 1, 2, 3 ]
+// 	.map(num => (
+// 		{
+// 			name: 'item' + num,
+// 			checked: false
+// 		})
+// 	)
+// ;
+
+let curItem;
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: '',
+			items: []
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({ value: event.target.value });
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		let createdItem = createItem(curItem);
+		let updatedItems = addItem(this.state.items, createdItem);
+		this.setState({ items: updatedItems });
+	}
+
+
 	render() {
 		return (
 			<div className="App">
 				<header className="App-header">
 					<h1 className="App-title">React to-do app</h1>
 				</header>
-				<InputForm />
-				<ListItems items={ITEMS} />
+
+				<form onSubmit={this.handleSubmit}>
+					<label>
+						Item:
+						<input
+							ref={node=> {
+								curItem = node;
+							}}
+							type="text"
+							value={this.state.value}
+							onChange={this.handleChange}
+						/>
+					</label>
+					<input type="submit" value="Submit" />
+				</form>
+
+				<ListItems items={this.state.items} />
 			</div>
 		);
 	}
--- src/src/list-items.js
+++ src/src/list-items.js
@@ -3,9 +3,9 @@ import * as React from 'react';
 const renderItems = (items) =>
 	items.map(
 		(item, num) =>
-		(
-		<li key={num}>Name: {item.name} Checked: {item.checked ? 'yes' : 'no'}</li>
-		)
+			(
+				<li key={num}>Name: {item.name} Checked: {item.checked ? 'yes' : 'no'}</li>
+			)
 	)
 ;
 
