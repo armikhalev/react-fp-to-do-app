eb80a91ede43ae0efd58589ba52133661fd2521f
armikhalev <armikhalev@gmail.com>
2018-02-11 22:58:14 -0500

    Fixes list-items wrong ul.

--- src/src/list-items.js
+++ src/src/list-items.js
@@ -5,15 +5,13 @@ const Container = children => (<div className="container">{children}</div>);
 const List = listItems => (<ul>{listItems}</ul>);
 const ListItem = ({ name, checked }) =>	(
 	<li key={name}>
-					Name: {name} Checked: {checked ? 'yes' : 'no'}
+		Name: {name} Checked: {checked ? 'yes' : 'no'}
 	</li>
 );
 
-const renderItems = R.pipe(R.map(ListItem), List, Container);
-
-/* eslint-disable react/prop-types */
-export const ListItems = (props) => {
-	return (
-		<ul>{renderItems(props.items)}</ul>
-	);
-};
+export const ListItems = R.pipe(
+	R.prop('items'),
+	R.map(ListItem),
+	List,
+	Container
+);
